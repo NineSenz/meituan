17806b599b8e8de49d51026ca5a22480892338ae
TimeSenz <1003823662@qq.com>
2020-03-08 21:21:54 +0800

    模版语法修改

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <div v-html="msg1" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Hello',
   props: {
     msg: String
   },
   data(){
     return{
-        msg1:'<span style="color:blue">Hello World</span>'
+        msg1:'<span style="color:red;">Hello World</span>'
     }
   }
 }
